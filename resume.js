299e09ef7eda524be82fd82cfc674ee49d788417
Vasu Jogani <vasujogani@Vasus-MBP.austin.rr.com>
2017-08-01 14:06:32 -0500

    implemented loop funct for right panel

--- resume.js
+++ resume.js
@@ -29,8 +29,43 @@ app.controller('listController', function($scope){
 app.controller('detailCntrl', function($scope){
 	$scope.msg="hel";
 	$scope.bio = "I’m an avid programmer and am seeking an internship opportunity for this summer to gain experience and apply programming skills I’ve gained. I will be attending University of Texas at Austin next fall and majoring in Computer Science. blah blah blah blah blah blah  blahblahblah blah blahblah blah blah blah blah blah blah  blahblahblah blah blahblah blah blah blah blah blah blah  blah blahblah blah blahblah blah blah blah blah blah blah  blah blahblah blah blahblah blah blah blah blah blah blah  blah blahblah blah  blah blah blah  blah blahblah blah blahblah blah blah blah blah blah blah ";
-	$scope.education=[
-		{name:"University of Texas at Austin", year: "2017-2021", desp: "ahblahblah blah blahblah blah blah blah blah blah blah"},
-		{name:"Round Rock High School", year: "2013-2017", desp:"I specialized in STEM with a 3.9 GPA. ahblahblah blah blahblah blah blah blah blah blah blah"}
+	$scope.everything=[
+		{//object1-- education
+			title: "Education",
+			topics: [{name:"University of Texas at Austin", year: "2017-2021", desp: "ahblahblah blah blahblah blah blah blah blah blah blah"},	{name:"Round Rock High School", year: "2013-2017", desp:"I specialized in STEM with a 3.9 GPA. ahblahblah blah blahblah blah blah blah blah blah blah"}]
+		},
+		{
+			title: "Education",
+			topics: [{name:"University of Texas at Austin", year: "2017-2021", desp: "ahblahblah blah blahblah blah blah blah blah blah blah"},	{name:"Round Rock High School", year: "2013-2017", desp:"I specialized in STEM with a 3.9 GPA. ahblahblah blah blahblah blah blah blah blah blah blah"}]
+		},
+		{
+			title: "Education",
+			topics: [{name:"University of Texas at Austin", year: "2017-2021", desp: "ahblahblah blah blahblah blah blah blah blah blah blah"},	{name:"Round Rock High School", year: "2013-2017", desp:"I specialized in STEM with a 3.9 GPA. ahblahblah blah blahblah blah blah blah blah blah blah"}]
+		},
+		{
+			title: "Education",
+			topics: [{name:"University of Texas at Austin", year: "2017-2021", desp: "ahblahblah blah blahblah blah blah blah blah blah blah"},	{name:"Round Rock High School", year: "2013-2017", desp:"I specialized in STEM with a 3.9 GPA. ahblahblah blah blahblah blah blah blah blah blah blah"}]
+		},
+		{
+			title: "Education",
+			topics: [{name:"University of Texas at Austin", year: "2017-2021", desp: "ahblahblah blah blahblah blah blah blah blah blah blah"},	{name:"Round Rock High School", year: "2013-2017", desp:"I specialized in STEM with a 3.9 GPA. ahblahblah blah blahblah blah blah blah blah blah blah"}]
+		},
+		{
+			title: "Education",
+			topics: [{name:"University of Texas at Austin", year: "2017-2021", desp: "ahblahblah blah blahblah blah blah blah blah blah blah"},	{name:"Round Rock High School", year: "2013-2017", desp:"I specialized in STEM with a 3.9 GPA. ahblahblah blah blahblah blah blah blah blah blah blah"}]
+		},
+		{
+			title: "Education",
+			topics: [{name:"University of Texas at Austin", year: "2017-2021", desp: "ahblahblah blah blahblah blah blah blah blah blah blah"},	{name:"Round Rock High School", year: "2013-2017", desp:"I specialized in STEM with a 3.9 GPA. ahblahblah blah blahblah blah blah blah blah blah blah"}]
+		},
+		{
+			title: "Education",
+			topics: [{name:"University of Texas at Austin", year: "2017-2021", desp: "ahblahblah blah blahblah blah blah blah blah blah blah"},	{name:"Round Rock High School", year: "2013-2017", desp:"I specialized in STEM with a 3.9 GPA. ahblahblah blah blahblah blah blah blah blah blah blah"}]
+		},
+		{}
+		
+	//	{title: "Education"},	
+	//	{name:"University of Texas at Austin", year: "2017-2021", desp: "ahblahblah blah blahblah blah blah blah blah blah blah"},
+	//	{name:"Round Rock High School", year: "2013-2017", desp:"I specialized in STEM with a 3.9 GPA. ahblahblah blah blahblah blah blah blah blah blah blah"}
 	];
 });
